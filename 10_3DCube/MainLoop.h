161a4cc8aa88c707389effe8e5f5de896eb5784d
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2020-03-01 23:10:50 +0100

    Add GLM library and add project rendering 3D cube.

---
+++ 10_3DCube/MainLoop.h
@@ -0,0 +1,18 @@
+#ifndef __MAIN_LOOP_H__
+#define __MAIN_LOOP_H__
+#include <SDL.h>
+#include <glad/glad.h>
+#include "RAII.h"
+
+class MainLoop
+{
+private:
+	SDL_Window* _window;
+	RAII<GLuint> _program;
+public:
+	MainLoop(SDL_Window* win);
+	void compile_program();
+	void loop();
+};
+
+#endif
